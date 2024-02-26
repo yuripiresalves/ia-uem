ad46432dbd7501a80a5035f2c4a0fc50254b4875
Yuri Alves <yuripiresalves@gmail.com>
2024-02-26 17:02:26 -0300

    refatora um pouco o codigo

--- 2/pln.py
+++ 2/pln.py
@@ -1,3 +1,6 @@
+import string
+import os
+
 from pypdf import PdfReader
 import nltk
 from nltk.corpus import stopwords
@@ -7,38 +10,30 @@ from nltk.stem.wordnet import WordNetLemmatizer
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
-import string
-import os
 
-# parts = []
+def write_in_file(text):
+    with open('results.txt', 'a') as f:
+        f.write(text + ';;')
 
-references = ''
-
-# def visitor_body(text, cm, tm, font_dict, font_size):
-#     isRefering = False
-#     references = ''
-#     parts = []
-#     y = cm[5]
-#     if isRefering:
-#         references += text
-#     if(text.strip() == 'REFERENCES'):
-#         print('entra')
-#         isRefering = True
-#     parts.append(text)
-#     print(references)
-
-def get_objective(txt):
+def get_references(txt):
+    references = ''
+    i = txt.find('REFERENCES')
+    references = txt[i+10:].strip()
+    write_in_file(references)
     
+def get_introduction(txt):
     start = txt.find('INTRODUCTION')
     end = txt.find('II.')
     
     if start != -1 and end != -1:
         introduction = txt[start+12:end].strip()
-        #print('Introdução: ' + introduction)
+        return introduction
     else:
-        #print('Introdução: Não encontrada')
         return
-
+    
+def get_objective(txt):
+    introduction = get_introduction(txt)
+    
     key_words = [
         "objective",
         "purpose",
@@ -58,10 +53,11 @@ def get_objective(txt):
     objectives = []
 
     for word in key_words:
-        i = introduction.find(word)
+        i = str(introduction).find(word)
         if i != -1:
             objectives.append(introduction[i:i+100])
-            print('Objetivo: ' + ','.join(objectives))
+            objective = ','.join(objectives)    
+            write_in_file(objective)
         else:
             break
 
@@ -74,11 +70,6 @@ def get_objective(txt):
     #else:
     #    print('Objetivo: Não encontrado')
 
-def getReferences(txt):
-    i = txt.find('REFERENCES')
-    references = txt[i+10:].strip()
-    # print(references)
-    # txt.split('REFERENCES')
 
 def get_problem(txt):
     start = txt.find('INTRODUCTION')
@@ -129,79 +120,76 @@ def main():
     # Lendo o arquivo PDF
     path = []
     pdfs = []
-    scandir = os.scandir('artigos/web')
-    for entry in scandir:
-        if entry.is_file():
-            path.append(entry.path)
-            pdfs.append(PdfReader(entry.path))
-    # reader = PdfReader("artigos/web/Strategies_for_web_application_development_methodologies.pdf")
-
-    parts = []
+    
+    articles = os.scandir('artigos/web')
+    for article in articles:
+        if article.is_file():
+            path.append(article.path)
+            pdfs.append(PdfReader(article.path))
 
     for idx, pdf in enumerate(pdfs):
-        print('-------------------------------------------------------------------------------')
+        print('------------------------------------------------------------')
         print('Article: ' + path[idx])
-       # print('Article: ' + pdf.metadata.title)
         reader = pdf
-        content = '' 
+        content = ''
 
         for page in reader.pages:
             content += page.extract_text()
 
-        getReferences(content)
+        get_references(content)
         get_objective(content) # Objective of the article
-        get_problem(content) # Problem of the article
+        # get_problem(content) # Problem of the article
         
-        content = content.split('REFERENCES')[0] #Remove the references from article
-        content = content.lower()
+        # content = content.split('REFERENCES')[0] #Remove the references from article
+        # content = content.lower()
 
-        # exit()
-        text = ''.join([c for c in content if c not in string.punctuation])
+        # # exit()
+        # text = ''.join([c for c in content if c not in string.punctuation])
 
 
-        # Tokenização
-        tokens = word_tokenize(text)
-        # print(tokens)
+        # # Tokenização
+        # tokens = word_tokenize(text)
+        # # print(tokens)
 
-        # Remoção de stopwords
-        stop_words = set(stopwords.words('english'))
-        tokens = [word for word in tokens if word.lower() not in stop_words]
-        # print(tokens)
+        # # Remoção de stopwords
+        # stop_words = set(stopwords.words('english'))
+        # tokens = [word for word in tokens if word.lower() not in stop_words]
+        # # print(tokens)
 
-        # Stemming
-        # stemmer = SnowballStemmer('english')
-        # tokens = [stemmer.stem(word) for word in tokens]
-        # print(tokens)
+        # # Stemming
+        # # stemmer = SnowballStemmer('english')
+        # # tokens = [stemmer.stem(word) for word in tokens]
+        # # print(tokens)
 
-        # Lematização
-        lemmatizer = WordNetLemmatizer()
-        tokens = [lemmatizer.lemmatize(word) for word in tokens]
-        # print(tokens)
+        # # Lematização
+        # lemmatizer = WordNetLemmatizer()
+        # tokens = [lemmatizer.lemmatize(word) for word in tokens]
+        # # print(tokens)
 
-        # Frequência
+        # # Frequência
 
-        freq = nltk.FreqDist(tokens)
-        #print('Frequência de palavras')
-        #print(freq.most_common(10))
-        #print('-------------------------------------')
-        # freq.plot(10, cumulative=False)
+        # freq = nltk.FreqDist(tokens)
+        # #print('Frequência de palavras')
+        # #print(freq.most_common(10))
+        # #print('-------------------------------------')
+        # # freq.plot(10, cumulative=False)
 
-        # # Bigramas
-        bigrams = list(nltk.bigrams(tokens))
-        # print(bigrams)
-        freq_bigrams = nltk.FreqDist(bigrams)
-        #print('Frequência de bigramas')
-        #print(freq_bigrams.most_common(10))
-        #print('-------------------------------------')
-        # freq_bigrams.plot(10, cumulative=False)
+        # # # Bigramas
+        # bigrams = list(nltk.bigrams(tokens))
+        # # print(bigrams)
+        # freq_bigrams = nltk.FreqDist(bigrams)
+        # #print('Frequência de bigramas')
+        # #print(freq_bigrams.most_common(10))
+        # #print('-------------------------------------')
+        # # freq_bigrams.plot(10, cumulative=False)
 
-        # # Trigramas
-        trigrams = list(nltk.trigrams(tokens))
-        # print(trigrams)
-        freq_trigrams = nltk.FreqDist(trigrams)
-        #print('Frequência de trigramas')
-        #print(freq_trigrams.most_common(10))
-        #print('-------------------------------------')
-        # freq_trigrams.plot(10, cumulative=False)
+        # # # Trigramas
+        # trigrams = list(nltk.trigrams(tokens))
+        # # print(trigrams)
+        # freq_trigrams = nltk.FreqDist(trigrams)
+        # #print('Frequência de trigramas')
+        # #print(freq_trigrams.most_common(10))
+        # #print('-------------------------------------')
+        # # freq_trigrams.plot(10, cumulative=False)
 
 main()
